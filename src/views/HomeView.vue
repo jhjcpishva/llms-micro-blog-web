39de2b6c2dd4cd2371e4c74c39744990f357d499
jhjcpishva <jhjcpishva@users.noreply.github.com>
2024-12-30 19:36:22 +0900

    play some tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-
 import TheWelcome from '@/components/TheWelcome.vue'
-
 </script>
 
 <template>
@@ -12,8 +10,18 @@ import TheWelcome from '@/components/TheWelcome.vue'
     <TheWelcome />
     <h2>Pages</h2>
     <ul>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink :to="{ name: 'login' }">Login</RouterLink></li>
+      <li class="pl-4">
+        <RouterLink class="link" to="/">Home</RouterLink>
+      </li>
+      <li class="pl-4">
+        <RouterLink class="link" :to="{ name: 'login' }">Login</RouterLink>
+      </li>
     </ul>
   </main>
 </template>
+
+<style scoped>
+.link {
+  @apply text-blue-600 hover:bg-gray-200 hover:underline;
+}
+</style>
